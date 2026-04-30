5305f557d423f02923ba24acaabc422076c30766
anastasiia-123 <uwumursifona@gmail.com>
2026-04-30 14:37:13 +0200

    feat: add cinema models

---
+++ backend/Models/Cinema.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CinemaApi.Models
+{
+    public class Cinema
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public List<Hall> Halls { get; set; } = new();
+    }
+}
