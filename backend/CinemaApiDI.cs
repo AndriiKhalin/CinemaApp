bd5b9aaae77e5775d45f4c69291046064e283466
AndriiKhalin <danoking2002@gmail.com>
2026-05-06 00:57:34 +0200

    Add CinemaApiDI class with service registration method
    Introduced the CinemaApiDI static class in the CinemaApi namespace. Added the AddCinemaApiServices extension method for IServiceCollection to centralize future CinemaApi service registrations. Currently includes a TODO for service registration.

---
+++ backend/CinemaApiDI.cs
@@ -0,0 +1,9 @@
+﻿namespace CinemaApi;
+
+public static class CinemaApiDI
+{
+    public static void AddCinemaApiServices(this IServiceCollection services)
+    {
+        // TODO: register services here (examples below):
+    }
+}
